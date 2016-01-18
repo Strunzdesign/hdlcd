aa085709b326ac10d04de80767356b55578f75ce
Florian Evers <florian-evers@gmx.de>
2016-01-18 19:46:21 +0100

    Fixed whitespace

--- src/hdlcd/HDLC/Frame.cpp
+++ src/hdlcd/HDLC/Frame.cpp
@@ -30,28 +30,29 @@ std::string Frame::GetReadableDescription() const {
     } else if (IsSFrame()) {
         l_Output << "HDLC frame, Addr=" << (int)GetAddress() << ", ";
         l_Output << "S-Frame: ";
-	switch (GetHDLCFrameType()) {
-	    case Frame::HDLC_FRAMETYPE_S_RR: {
-	        l_Output << "RR";
-		break;
-	    }
-	    case Frame::HDLC_FRAMETYPE_S_RNR: {
-	        l_Output << "RNR";
-		break;
-	    }
-	    case Frame::HDLC_FRAMETYPE_S_REJ: {
-	        l_Output << "REJ";
-		break;
-	    }
-	    case Frame::HDLC_FRAMETYPE_S_SREJ: {
-	        l_Output << "SREJ";
-		break;
-	    }
-	    default: {
-	        break;
-	    }
-	} // switch
-	l_Output << ", PF=" << IsPF() << ", RSeq=" << (int)GetRSeq();
+        switch (GetHDLCFrameType()) {
+            case Frame::HDLC_FRAMETYPE_S_RR: {
+                l_Output << "RR";
+                break;
+            }
+            case Frame::HDLC_FRAMETYPE_S_RNR: {
+                l_Output << "RNR";
+                break;
+            }
+            case Frame::HDLC_FRAMETYPE_S_REJ: {
+                l_Output << "REJ";
+                break;
+            }
+            case Frame::HDLC_FRAMETYPE_S_SREJ: {
+                l_Output << "SREJ";
+                break;
+            }
+            default: {
+                break;
+            }
+        } // switch
+
+        l_Output << ", PF=" << IsPF() << ", RSeq=" << (int)GetRSeq();
     } else if (IsUFrame()) {
         l_Output << "HDLC frame, Addr=" << (int)GetAddress() << ", ";
         l_Output << "U-Frame, PF=" << IsPF();
