6de390d1808f13008dc76f9f3c1aa725be917901
Florian Evers <florian-evers@gmx.de>
2016-07-25 22:58:46 +0200

    Added doxygen headers to file AccessClient.h

--- src/shared/AccessClient.h
+++ src/shared/AccessClient.h
@@ -1,6 +1,6 @@
 /**
- * \file AccessClient.h
- * \brief 
+ * \file  AccessClient.h
+ * \brief This file contains the header declaration of class AccessClient
  *
  * The hdlc-tools implement the HDLC protocol to easily talk to devices connected via serial communications
  * Copyright (C) 2016  Florian Evers, florian-evers@gmx.de
@@ -29,9 +29,23 @@
 #include "PacketData.h"
 #include "PacketCtrl.h"
 
+/*! \class AccessClient
+ *  \brief Class AccessClient
+ * 
+ *  The main helper class to easily implement clients of the HDLCd access protocol
+ */
+
 class AccessClient {
 public:
-    // CTOR
+    /*! \brief The constructor of class AccessClient
+     * 
+     *  The connection is established on instantiation (RAII)
+     * 
+     *  \param a_IoService the boost IOService object
+     *  \param a_EndpointIterator the boost endpoint iteratior referring to the destination
+     *  \param a_SerialPortName the name of the serial port device
+     *  \param a_SAP the numerical indentifier of the service access protocol
+     */
     AccessClient(boost::asio::io_service& a_IoService, boost::asio::ip::tcp::resolver::iterator a_EndpointIterator, std::string a_SerialPortName, unsigned char a_SAP):
         m_bClosed(false),
         m_TCPDataSocket(a_IoService),
@@ -75,19 +89,31 @@ public:
             } // else
         });
     }
-    
+
+    /*! \brief The destructor of class AccessClient
+     * 
+     *  All open connections will automatically be closed by the destructor
+     */
     ~AccessClient() {
         m_OnDataCallback = NULL;
         m_OnCtrlCallback = NULL;
         m_OnClosedCallback = NULL;
         Close();
     }
-    
+
+    /*! \brief Shutdowns all TCP connections down
+     * 
+     *  Initiates a shutdown procedure for correct teardown of all TCP connections
+     */
     void Shutdown() {
         m_PacketEndpointData->Shutdown();
         m_PacketEndpointCtrl->Shutdown();
     }
 
+    /*! \brief Close the client entity
+     * 
+     *  Close the client entity and all its TCP connections
+     */
     void Close() {
         if (m_bClosed == false) {
             m_bClosed = true;
@@ -111,29 +137,61 @@ public:
         } // if
     }
     
-    // Callback methods
+    /*! \brief Provide a callback method to be called for received data packets
+     * 
+     *  Data packets are received in an asynchronous way. Use this method to specify a callback method to be called on reception of single data packets
+     * 
+     *  \param a_OnDataCallback the funtion pointer to the callback method, may be an empty function pointer to remove the callback
+     */
     void SetOnDataCallback(std::function<void(const PacketData& a_PacketData)> a_OnDataCallback) {
         m_OnDataCallback = a_OnDataCallback;
     }
     
+    /*! \brief Provide a callback method to be called for received control packets
+     * 
+     *  Control packets are received in an asynchronous way. Use this method to specify a callback method to be called on reception of single control packets
+     * 
+     *  \param a_OnCtrlCallback the funtion pointer to the callback method, may be an empty function pointer to remove the callback
+     */
     void SetOnCtrlCallback(std::function<void(const PacketCtrl& a_PacketCtrl)> a_OnCtrlCallback) {
         m_OnCtrlCallback = a_OnCtrlCallback;
     }
     
+    /*! \brief Provide a callback method to be called if this client entity is closing
+     * 
+     *  This method can be used to provide a function pointer callback to be called if this entoty is closing, e.g., the peer closed its endpoint
+     * 
+     *  \param a_OnClosedCallback the funtion pointer to the callback method, may be an empty function pointer to remove the callback
+     */
     void SetOnClosedCallback(std::function<void()> a_OnClosedCallback) {
         m_OnClosedCallback = a_OnClosedCallback;
     }
     
+    /*! \brief Send a single data packet to the peer entity
+     * 
+     *  Send a single data packet to the peer entity
+     * 
+     *  \param a_PacketData the data packet to be transmitted
+     */
     bool Send(const PacketData& a_PacketData) {
         return m_PacketEndpointData->Send(&a_PacketData);
     }
 
+    /*! \brief Send a single control packet to the peer entity
+     * 
+     *  Send a single control packet to the peer entity
+     * 
+     *  \param a_PacketCtrl the control packet to be transmitted
+     */
     bool Send(const PacketCtrl& a_PacketCtrl) {
         return m_PacketEndpointCtrl->Send(&a_PacketCtrl);
     }
     
 private:
-    // Internal callback methods
+    /*! \brief Internal helper method to write the session headers to both outgoing TCP sockets
+     * 
+     *  This is an internal helper method to write the session headers to both outgoing TCP sockets
+     */
     void WriteSessionHeaders() {
         if ((!m_bDataSocketConnected) || (!m_bCtrlSocketConnected)) {
             return;
@@ -180,6 +238,12 @@ private:
         });
     }
     
+    /*! \brief Internal callback method to be called on reception of data packets
+     * 
+     *  This is an internal callback method to be called on reception of data packets
+     * 
+     *  \param a_PacketData the received data packet
+     */
     bool OnDataReceived(std::shared_ptr<const PacketData> a_PacketData) {
         if (m_OnDataCallback) {
             m_OnDataCallback(*(a_PacketData.get()));
@@ -187,37 +251,47 @@ private:
         
         return true; // Do not stall the receiver
     }
-    
+
+    /*! \brief Internal callback method to be called on reception of control packets
+     * 
+     *  This is an internal callback method to be called on reception of control packets
+     * 
+     *  \param a_PacketCtrl the received data packet
+     */
     void OnCtrlReceived(const PacketCtrl& a_PacketCtrl) {
         if (m_OnCtrlCallback) {
             m_OnCtrlCallback(a_PacketCtrl);
         } // if
     }
-    
+
+    /*! \brief Internal callback method to be called on close of one of the TCP sockets
+     * 
+     *  This is an internal callback method to be called on close of one of the TCP sockets
+     */
     void OnClosed() {
         Close();
     }
     
     // Members
-    unsigned char m_SAP;
-    bool m_bClosed;
-    boost::asio::ip::tcp::socket m_TCPDataSocket;
-    boost::asio::ip::tcp::socket m_TCPCtrlSocket;
-    bool m_bDataSocketConnected;
-    bool m_bCtrlSocketConnected;
+    unsigned char m_SAP; //! The service access point identifier that specifies the session type
+    bool m_bClosed; //! Indicates whether the HDLCd access protocol entity has already been closed
+    boost::asio::ip::tcp::socket m_TCPDataSocket; //! The TCP connection dedicated to user data
+    boost::asio::ip::tcp::socket m_TCPCtrlSocket; //! The TCP connection dedicated to control data
+    bool m_bDataSocketConnected; //! This flag indicates whether the user data socket is established
+    bool m_bCtrlSocketConnected; //! This flag indicates whether the user control socket is established
     
     // Both session headers to be transmitted
-    std::vector<unsigned char> m_SessionHeaderData;
-    std::vector<unsigned char> m_SessionHeaderCtrl;
+    std::vector<unsigned char> m_SessionHeaderData; //! The buffer containing the session header for the data socket
+    std::vector<unsigned char> m_SessionHeaderCtrl; //! The buffer containing the session header for the control socket
     
-    std::string m_SerialPortName;
-    std::shared_ptr<PacketEndpoint> m_PacketEndpointData;
-    std::shared_ptr<PacketEndpoint> m_PacketEndpointCtrl;
+    std::string m_SerialPortName; //! The name of the serial port device
+    std::shared_ptr<PacketEndpoint> m_PacketEndpointData; //! The packet endpoint class responsible for the connected data socket
+    std::shared_ptr<PacketEndpoint> m_PacketEndpointCtrl; //! The packet endpoint class responsible for the connected control socket
     
     // All possible callbacks for a user of this class
-    std::function<void(const PacketData&)> m_OnDataCallback;
-    std::function<void(const PacketCtrl&)> m_OnCtrlCallback;
-    std::function<void()> m_OnClosedCallback;
+    std::function<void(const PacketData&)> m_OnDataCallback; //! The callback function that is invoked on reception of a data packet
+    std::function<void(const PacketCtrl&)> m_OnCtrlCallback; //! The callback function that is invoked on reception of a control packet
+    std::function<void()> m_OnClosedCallback;  //! The callback function that is invoked if the either this endpoint or that of the peer goes down
 };
 
 #endif // ACCESS_CLIENT_H
