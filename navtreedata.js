ff064a0569bf283685ed6ffe797c8f46d088067e
Florian Evers <florian-evers@gmx.de>
2016-08-21 19:33:26 +0200

    Changed settings to reflect new name of the repository

--- navtreedata.js
+++ navtreedata.js
@@ -1,6 +1,6 @@
 var NAVTREE =
 [
-  [ "HDLC-Tools", "index.html", [
+  [ "HDLCDaemon", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
