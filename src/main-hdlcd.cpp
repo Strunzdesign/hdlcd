fbd5151bbf6942d92e7e55c5ea3aa80205857272
Florian Evers <florian-evers@gmx.de>
2016-08-28 21:19:22 +0200

    Added command line handling to the HDLC daemon

--- src/main-hdlcd.cpp
+++ src/main-hdlcd.cpp
@@ -1,5 +1,5 @@
 /**
- * \file main.cpp
+ * \file main-hdlcd.cpp
  * \brief 
  *
  * The hdlc-tools implement the HDLC protocol to easily talk to devices connected via serial communications
@@ -22,25 +22,56 @@
 #include "Config.h"
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 #include "SerialPort/SerialPortHandlerCollection.h"
 #include "AccessProtocol/ClientAcceptor.h"
 
 int main(int argc, char **argv) {
-    std::cerr << "HDLC daemon v" << HDLCD_VERSION_MAJOR << "." << HDLCD_VERSION_MINOR
-              << " built with hdlcd-devel v" << HDLCD_DEVEL_VERSION_MAJOR << "." << HDLCD_DEVEL_VERSION_MINOR << std::endl;
-    if (argc != 2) {
-        std::cerr << "Usage: hdlcd <TCP port>\n";
-        return 1;
-    } // if
+    try {
+        // Declare the supported options.
+        boost::program_options::options_description l_Description("Allowed options");
+        l_Description.add_options()
+            ("help,h", "produce this help message")
+            ("version,v", "show version information")
+            ("port,p", boost::program_options::value<uint16_t>(), "the TCP port to accept clients on")
+        ;
+
+        // Parse the command line
+        boost::program_options::variables_map l_VariablesMap;
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, l_Description), l_VariablesMap);
+        boost::program_options::notify(l_VariablesMap);
+        if (l_VariablesMap.count("version")) {
+            std::cerr << "HDLC daemon v" << HDLCD_VERSION_MAJOR << "." << HDLCD_VERSION_MINOR
+                      << " built with hdlcd-devel v" << HDLCD_DEVEL_VERSION_MAJOR << "." << HDLCD_DEVEL_VERSION_MINOR << std::endl;
+        } // if
+
+        if (l_VariablesMap.count("help")) {
+            std::cout << l_Description << std::endl;
+            return 1;
+        } // if
+                        
+        if (!l_VariablesMap.count("port")) {
+            std::cout << "you have to specify the TCP listener port" << std::endl;
+            return 1;
+        } // if
+
+        // Install signal handlers
+        boost::asio::io_service l_IoService;
+        boost::asio::signal_set l_Signals(l_IoService);
+        l_Signals.add(SIGINT);
+        l_Signals.add(SIGTERM);
+        l_Signals.async_wait([&l_IoService](boost::system::error_code a_ErrorCode, int a_SignalNumber){ l_IoService.stop(); });
         
-    boost::asio::io_service io_service;
-    boost::asio::signal_set signals_(io_service);
-    signals_.add(SIGINT);
-    signals_.add(SIGTERM);
-    signals_.async_wait([&io_service](boost::system::error_code a_ErrorCode, int a_SignalNumber){io_service.stop();});
+        // Create the HDLCd entity
+        auto l_SerialPortHandlerCollection = std::make_shared<SerialPortHandlerCollection>(l_IoService);
+        ClientAcceptor l_ClientAcceptor(l_IoService, l_VariablesMap["port"].as<uint16_t>(), l_SerialPortHandlerCollection);
+        
+        // Start event processing
+        l_IoService.run();
+    } catch (std::exception& a_ErrorCode) {
+        std::cerr << "Exception: " << a_ErrorCode.what() << "\n";
+        return 1;
+    } // catch
     
-    auto l_SerialPortHandlerCollection = std::make_shared<SerialPortHandlerCollection>(io_service);
-    ClientAcceptor l_ClientAcceptor(io_service, atoi(argv[1]), l_SerialPortHandlerCollection);
-    io_service.run(); 
     return 0;
 }
